0466d0af433f715a88cdd87bbe55179bd9665e12
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-06-01 21:53:02 -0300

    adding natural language understanding api

--- backend/src/controllers/Tweets.js
+++ backend/src/controllers/Tweets.js
@@ -1,17 +1,51 @@
-const Tweet = require('../models/Tweet')
+const Tweet = require("../models/Tweet");
+const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1.js");
 
 module.exports = {
-  async index (req, res) {
-    const tweets = await Tweet.find({}).sort('-createdAt')
+  async index(_, res) {
+    const tweets = await Tweet.find({}).sort("-createdAt");
 
-    return res.json(tweets)
+    return res.json(tweets);
   },
 
-  async store (req, res) {
-    const tweet = await Tweet.create(req.body)
+  async store(req, res) {
+    const emotions = [];
+    const tags = ["sadness", "joy", "fear", "anger"];
 
-    req.io.emit('tweet', tweet)
-    
-    return res.json(tweet)
+    const nlu = new NaturalLanguageUnderstandingV1({
+      iam_apikey: process.env.NATURAL_LANGUAGE_UNDERSTANDING_IAM_APIKEY,
+      version: "2018-04-05",
+      url: process.env.NATURAL_LANGUAGE_UNDERSTANDING_URL
+    });
+    nlu
+      .analyze({
+        html: req.body.content,
+        features: {
+          emotion: {}
+        }
+      })
+      .then(result => {
+        const {
+          emotion: {
+            document: {
+              emotion: { sadness, joy, fear, anger }
+            }
+          }
+        } = result;
+
+        [sadness, joy, fear, anger].map((emotion, i) => {
+          if (emotion > 0.3) emotions.push(tags[i]);
+        });
+
+        console.log(emotions);
+      })
+      .catch(err => {
+        console.log("error:", err);
+      });
+    const tweet = await Tweet.create(req.body);
+
+    req.io.emit("tweet", tweet);
+
+    return res.json(tweet);
   }
-}
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,28 +1,34 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const routes = require('./routes')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const app = express()
+require("dotenv").config();
 
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const app = express();
 
-const port = process.env.PORT || 5000
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const port = process.env.PORT || 5000;
 
 mongoose.connect(
-  'mongodb://goweek:goweek123@ds155213.mlab.com:55213/goweek-4',
+  "mongodb://goweek:goweek123@ds155213.mlab.com:55213/goweek-4",
   { useNewUrlParser: true }
-)
+);
 
 app.use((req, res, next) => {
-  req.io = io
-  return next()
-})
-app.use(cors())
-app.use(express.json())
-app.use(routes)
+  req.io = io;
+  return next();
+});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
