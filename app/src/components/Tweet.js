2da2132f09865209b0fe6058bf359598dc3578c6
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-06-01 23:33:28 -0300

    pretty when you cry

--- app/src/components/Tweet.js
+++ app/src/components/Tweet.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import api from '../services/api'
+import React, { Component } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import api from "../services/api";
 
-import Icon from 'react-native-vector-icons/Ionicons'
+import Icon from "react-native-vector-icons/Ionicons";
 
 export default class Tweet extends Component {
   handleLike = async () => {
-    const { tweet } = this.props
+    const { tweet } = this.props;
 
-    await api.post(`likes/${tweet._id}`)
-  }
+    await api.post(`likes/${tweet._id}`);
+  };
 
-  render () {
-    const { tweet } = this.props
+  render() {
+    const { tweet } = this.props;
 
     return (
       <View style={styles.container}>
@@ -20,11 +20,11 @@ export default class Tweet extends Component {
         <Text style={styles.content}>{tweet.content}</Text>
 
         <TouchableOpacity onPress={this.handleLike} style={styles.likeButton}>
-          <Icon name="ios-heart-empty" size={20} color="#999"/>
+          <Icon name="ios-heart-empty" size={20} color="#999" />
           <Text style={styles.likeText}>{tweet.likes}</Text>
         </TouchableOpacity>
       </View>
-    )
+    );
   }
 }
 
